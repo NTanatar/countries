2b9fcd0f10f8611f6d810e3e5a4d8ca6dbd1ed47
Natalia Tanatarova <natalia.tanatarova@sigma.software>
2021-03-03 23:55:30 +0200

    Implementing repositories using JDBC, with RowMappers as Beans and tests in an ApplicationReadyEvent listener.

--- src/main/java/com/tannat/country/domain/City.java
+++ src/main/java/com/tannat/country/domain/City.java
@@ -1,10 +1,12 @@
 package com.tannat.country.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 public class City {
     private Long id;
     private Long countryId;
---
+++ src/main/java/com/tannat/country/repositories/impl/CityRepositoryImpl.java
@@ -0,0 +1,92 @@
+package com.tannat.country.repositories.impl;
+
+import com.tannat.country.domain.City;
+import com.tannat.country.repositories.CityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class CityRepositoryImpl implements CityRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<City> mapper;
+
+    @Override
+    public City getById(Long id) {
+        return jdbcTemplate.queryForObject("select * from cities where id = ?", mapper, id);
+    }
+
+    @Override
+    public List<City> getAll() {
+        return jdbcTemplate.query("select * from cities", mapper);
+    }
+
+    @Override
+    public City add(City c) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(conn -> createInsertStatement(conn, c), keyHolder);
+        return getById((Long) Objects.requireNonNull(keyHolder.getKeys()).get("id"));
+    }
+
+    @Override
+    public City update(City c) {
+        jdbcTemplate.update(conn -> createUpdateStatement(conn, c));
+        return getById(c.getId());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        jdbcTemplate.update("delete from cities where id = ?", id);
+    }
+
+
+    private PreparedStatement createInsertStatement(Connection conn, City c) throws SQLException {
+        String sql = "insert into cities " +
+                "(name, country_id, founding_date, city_day, has_river, population) " +
+                "values (?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        fillParameters(ps, c, false);
+        return ps;
+    }
+
+    private PreparedStatement createUpdateStatement(Connection conn, City c) throws SQLException {
+        String sql = "update cities set " +
+                "name = ?, country_id = ?, founding_date = ?, city_day = ?, has_river = ?, population = ?" +
+                "where id = ?";
+
+        PreparedStatement ps = conn.prepareStatement(sql);
+        fillParameters(ps, c, true);
+        return ps;
+    }
+
+    private void fillParameters(PreparedStatement ps, City c, boolean setId) throws SQLException {
+        ps.setString(1, c.getName());
+        ps.setLong(2, c.getCountryId());
+
+        Date fd = Optional.ofNullable(c.getFoundingDate()).map(Date::valueOf).orElse(null);
+        ps.setDate(3, fd);
+
+        Date cd = Optional.ofNullable(c.getCityDay()).map(Date::valueOf).orElse(null);
+        ps.setDate(4, cd);
+
+        ps.setBoolean(5, c.getHasRiver());
+        ps.setInt(6, c.getPopulation());
+        if (setId) {
+            ps.setLong(7, c.getId());
+        }
+    }
+
+}
+
